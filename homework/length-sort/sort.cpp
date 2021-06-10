a703f3a0cc0ae06b4d6d183cb2177122f2aa0420
Wojtek Płóciennik <wojtek.plociennik@gmail.com>
2021-06-10 23:52:31 +0200

    Homework stl sort - length implemented

---
+++ homework/length-sort/sort.cpp
@@ -0,0 +1,14 @@
+#include "sort.hpp"
+
+bool compare_length(std::string& str1, std::string& str2) {
+    if (str1.length() < str2.length() || str1.length() == str2.length() && str1 < str2) {
+        return true;
+    }
+    return false;
+}
+
+std::deque<std::string> lengthSort(std::forward_list<std::string> flist) {
+    std::deque<std::string> deq(flist.begin(), flist.end());
+    std::sort(deq.begin(), deq.end(), compare_length);
+    return deq;
+}
