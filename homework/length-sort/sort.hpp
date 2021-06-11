abd07e937b3d467c1903dec188f2d5b6c15bd952
Wojtek Płóciennik <wojtek.plociennik@gmail.com>
2021-06-11 19:02:39 +0200

    "checking workin GH"

--- homework/length-sort/sort.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <algorithm>
-#include <deque>
-#include <forward_list>
-#include <iostream>
-
-std::deque<std::string> lengthSort(std::forward_list<std::string> flist);
-
-bool compare_length(std::string& str1, std::string& str2);
