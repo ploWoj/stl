fb9c6ffb9b87ca99d8c978b85fc2ab825425985e
Wojtek Płóciennik <wojtek.plociennik@gmail.com>
2021-06-11 19:05:51 +0200

    what is this

---
+++ homework/remove-vowels/vowel.cpp
@@ -0,0 +1,13 @@
+#include "vowel.hpp"
+#include <algorithm>
+
+void removeVowels(std::vector<std::string>& words) {
+  std::string vowels = "aeiouyAEIOUY";
+
+   for (int i = 0; i < vowels.length(); i++) {
+       for (auto& el : words) {
+         el.erase(std::remove(el.begin(), el.end(), vowels[i]),
+                   el.end());
+     }
+}
+}
---
+++ homework/remove-vowels/vowel.hpp
@@ -0,0 +1,7 @@
+#pragma once 
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+void removeVowels(std::vector<std::string>& vec);
